4a0f58450e5aaadb6d1712b7617a62c0d1f1005c
Ekaterina Voronina <vor@altlinux.org>
2023-08-07 18:34:06 +0400

    feat: add algorithm astar

---
+++ astar.h
@@ -0,0 +1,23 @@
+#ifndef ASTAR_H
+#define ASTAR_H
+#include "astar.h"
+#include "node.h"
+#include "graph.h"
+#include <queue>
+
+class Astar
+{
+public:
+    Astar();
+    QString run(Node* start, Node* goal, Graph& graph);
+    QString restorePath(Node* start, Node* goal);
+
+private:
+    std::vector<Node*> queue;
+    std::vector<Node*> visited;
+    std::unordered_map<Node*, Node*> parent;
+    std::unordered_map<Node*, int> minWay;
+
+};
+
+#endif // ASTAR_H
