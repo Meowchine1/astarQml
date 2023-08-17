d2661572237ba57736d16943bc5dc75bb7c69511
Ekaterina Voronina <vor@altlinux.org>
2023-08-17 17:00:00 +0400

    refactoring

--- astar.h
+++ astar.h
@@ -1,16 +1,17 @@
 #ifndef ASTAR_H
 #define ASTAR_H
+
+#include <queue>
+
 #include "astar.h"
 #include "node.h"
 #include "graph.h"
-#include <queue>
 
 class Astar
 {
 public:
     Astar();
     QString run(Node* start, Node* goal, Graph& graph);
-
 
 private:
     std::vector<Node*> queue;
